ae214d20e45d5a10a4556ab3cb8757af9ea28bf7
dinto <dinto.t.d@gmail.com>
2024-01-21 23:12:34 +0530

    ecommerce page updated

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -13,8 +13,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'EcommerceApp',
+    'widget_tweaks',
     
 ]
 
@@ -56,7 +60,8 @@ ROOT_URLCONF = 'Ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        #'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,7 +83,8 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -118,12 +124,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT= os.path.join(BASE_DIR,'static')
 
-STATICFILES_DIRS= [os.path.join(BASE_DIR,'staticfiles')]
+STATICFILES_DIRS=[STATIC_DIR,]
 
-MEDIA_URL='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+LOGIN_REDIRECT_URL='/afterlogin'
+
+#STATIC_ROOT= os.path.join(BASE_DIR,'static')
+
+
+#STATICFILES_DIRS= [os.path.join(BASE_DIR,'staticfiles')]
+
+#MEDIA_URL='/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
